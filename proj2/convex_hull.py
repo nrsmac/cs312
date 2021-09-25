8d04ec9b7033415da9ec163a75d84fcdadccccd0
nrsmac <noahrschill@gmail.com>
2021-09-24 18:00:37 -0600

    moving to linux

--- proj2/convex_hull.py
+++ proj2/convex_hull.py
@@ -1,14 +1,14 @@
 from which_pyqt import PYQT_VER
 if PYQT_VER == 'PYQT5':
-	from PyQt5.QtCore import QLineF, QPointF, QObject
+    from PyQt5.QtCore import QLineF, QPointF, QObject
 elif PYQT_VER == 'PYQT4':
-	from PyQt4.QtCore import QLineF, QPointF, QObject
+    from PyQt4.QtCore import QLineF, QPointF, QObject
 else:
-	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
+    raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 
 
-import time
+import time 
 
 # Some global color constants that might be useful
 RED = (255,0,0)
@@ -25,58 +25,57 @@ PAUSE = 0.25
 class ConvexHullSolver(QObject):
 
 # Class constructor
-	def __init__( self):
-		super().__init__()
-		self.pause = False
-		
+    def __init__( self):
+        super().__init__()
+        self.pause = False
+
 # Some helper methods that make calls to the GUI, allowing us to send updates
 # to be displayed.
 
-	def showTangent(self, line, color):
-		self.view.addLines(line,color)
-		if self.pause:
-			time.sleep(PAUSE)
+    def showTangent(self, line, color):
+        self.view.addLines(line,color)
+        if self.pause:
+            time.sleep(PAUSE)
 
-	def eraseTangent(self, line):
-		self.view.clearLines(line)
+    def eraseTangent(self, line):
+        self.view.clearLines(line)
 
-	def blinkTangent(self,line,color):
-		self.showTangent(line,color)
-		self.eraseTangent(line)
+    def blinkTangent(self,line,color):
+        self.showTangent(line,color)
+        self.eraseTangent(line)
 
-	def showHull(self, polygon, color):
-		self.view.addLines(polygon,color)
-		if self.pause:
-			time.sleep(PAUSE)
-		
-	def eraseHull(self,polygon):
-		self.view.clearLines(polygon)
-		
-	def showText(self,text):
-		self.view.displayStatusText(text)
-	
+    def showHull(self, polygon, color):
+        self.view.addLines(polygon,color)
+        if self.pause:
+            time.sleep(PAUSE)
+
+    def eraseHull(self,polygon):
+        self.view.clearLines(polygon)
+
+    def showText(self,text):
+        self.view.displayStatusText(text)
+
 
 # This is the method that gets called by the GUI and actually executes
 # the finding of the hull
-	def compute_hull( self, points, pause, view):
-		self.pause = pause
-		self.view = view
-		assert( type(points) == list and type(points[0]) == QPointF )
+    def compute_hull( self, points, pause, view):
+        self.pause = pause
+        self.view = view
+        assert( type(points) == list and type(points[0]) == QPointF )
 
-		t1 = time.time()
-		# TODO: SORT THE POINTS BY INCREASING X-VALUE
-		t2 = time.time()
+        t1 = time.time()
+        # TODO: SORT THE POINTS BY INCREASING X-VALUE
+        
+        t2 = time.time()
 
-		t3 = time.time()
-		# this is a dummy polygon of the first 3 unsorted points
-		polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
-		# TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
-		t4 = time.time()
+        t3 = time.time()
+        # this is a dummy polygon of the first 3 unsorted points
+        polygon = [QLineF(points[i],points[(i+1)%3]) for i in range(3)]
+        # TODO: REPLACE THE LINE ABOVE WITH A CALL TO YOUR DIVIDE-AND-CONQUER CONVEX HULL SOLVER
+        t4 = time.time()
 
-		# when passing lines to the display, pass a list of QLineF objects.  Each QLineF
-		# object can be created with two QPointF objects corresponding to the endpoints
-		self.showHull(polygon,RED)
-		self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
-
-
-
+        # when passing lines to the display, pass a list of QLineF objects.  Each QLineF
+        # object can be created with two QPointF objects corresponding to the endpoints
+        from pdb4qt import set_trace; set_trace()
+        self.showHull(polygon,RED)
+        self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4-t3))
